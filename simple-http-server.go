145f0d2dc904eb6f9f446d904cd6314d30bb668d
CindyXing0315 <xingli0315@gmail.com>
2017-11-29 11:40:24 +0100

    Create simple-http-server.go file

---
+++ simple-http-server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+
+  fmt.Fprintf(w, "Hello world!")
+}
+
+func main() {
+
+  port := os.Getenv("HTTP_PORT")
+
+  if port == "" {
+    port = ":9000"
+  }
+
+  fmt.Println("Started server")
+  http.HandleFunc("/hello", HelloWorld)
+  http.ListenAndServe(port, nil)
+}
